5339e9486595c186025a70cae31da422400208a3
박희진 <heejinpark10@gmail.com>
2022-06-15 23:07:34 +0900

    useState

--- src/Components/ApiCall.js
+++ src/Components/ApiCall.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
+//import React, {Component} from 'react';
 
 
 const ApiCall = () => {
