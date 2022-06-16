8e25781a5d4a54c17a1941c437b689a876827d28
박희진 <heejinpark10@gmail.com>
2022-06-16 23:22:03 +0900

    useEffect

--- src/Components/ApiCall.js
+++ src/Components/ApiCall.js
@@ -5,7 +5,7 @@ import React, {useState, useEffect} from 'react';
 
 const ApiCall = () => {
   axios.defaults.withCredentials = true;
-    
+      useEffect(()=> {
       const response = axios.get(
         'http://localhost:3000/', 
         'https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttbtogether0002243001&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20131101'
@@ -13,8 +13,9 @@ const ApiCall = () => {
         {
           withCredentials:true  // 쿠키 cors 통신 설정
         }).then(response => {
-            console.log(response);
+            console.log(response.data);
           });
+        });
     }  
 
 
