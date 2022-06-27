e9628bb385119246ee5c56d1c8a6b029580acf24
박희진 <heejinpark10@gmail.com>
2022-06-27 23:10:48 +0900

    Objects are not valid

--- src/Components/ApiCall.js
+++ src/Components/ApiCall.js
@@ -3,16 +3,13 @@ import { useEffect } from 'react';
 import asset from '../asset.js';
 //import React, {useState, useEffect} from 'react';
 
-function ApiConnect(){
-const ApiCall = () => {
+const ApiCall = async() => {
   const apiKey = asset.apiKey;  
-  const request = axios.get(
+  const request = await axios.get(
     `http://localhost:3000/`, 
     `https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${apiKey}&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20131101`
     )
-    useEffect(() => { 
-      ApiCall();
-    }, []);
+    console.log(request);
   }
-}  
-export default ApiConnect;
+ 
+export default ApiCall;
