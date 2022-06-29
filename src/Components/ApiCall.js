29e7991abd0e7a420abae37b82b37d1f11f4c9ff
박희진 <heejinpark10@gmail.com>
2022-06-29 23:16:13 +0900

    useEffect useState

--- src/Components/ApiCall.js
+++ src/Components/ApiCall.js
@@ -1,16 +1,24 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useState ,useEffect } from 'react';
 import asset from '../asset.js';
 //import React, {useState, useEffect} from 'react';
 
-async function ApiCall(){
-  const apiKey = asset.apiKey;  
-  const request = axios.get(
-    `http://localhost:3000/`, 
-    `https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${apiKey}&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20131101`
-    )
-    console.log(request.data)
-//const ApiCall = () => {
-  //}
+const ApiCall = () => {
+  const [data, setData] = useState({});
+  const apiKey = asset.apiKey; 
+  useEffect(() => {
+    const getApi = async() => {
+      const request = await axios.get(
+        `http://localhost:3000/`, 
+        `https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${apiKey}&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20131101`
+      )
+      const requestJson = await request.json();
+      console.log("json", requestJson);
+      setData(requestJson);
+    };
+    getApi();
+  }, []);
+
+    console.log(data)
 }
 export default ApiCall;
