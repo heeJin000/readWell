750ab6655ea6616618c65ec98d059e69cd21424b
박희진 <heejinpark10@gmail.com>
2022-07-06 22:34:48 +0900

    finding XML

--- src/Components/ApiCall.js
+++ src/Components/ApiCall.js
@@ -4,24 +4,12 @@ import asset from '../asset.js';
 
 
 const ApiCall = () => {
-  const [data, setData] = useState({});
-  const apiKey = asset.apiKey; 
-
-  useEffect(() => {
-    const getApi = async() => {
-      const request = await axios.get(
-        `http://localhost:3000/`, 
-        `https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${apiKey}&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20131101`
-      )
-      const requestJson = request.data;
-
-      if(!requestJson) alert('데이터가 존재하지 않습니다.');
-      else{
-      console.log(requestJson);
-      setData(requestJson);
-      }
-    };
-    getApi();
-  }, []);
-}
+  const apiKey = asset.apiKey;  
+  const response = axios.get(
+    `http://localhost:3000/`, 
+    `https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${apiKey}&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20131101`
+    ).then(response => {
+        console.log(response.data);
+      });
+}  
 export default ApiCall;
