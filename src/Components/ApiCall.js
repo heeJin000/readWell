62235004484b713f734c43f93cc249f90cb97b7e
박희진 <heejinpark10@gmail.com>
2022-07-12 22:35:01 +0900

    AxiosResponse

--- src/Components/ApiCall.js
+++ src/Components/ApiCall.js
@@ -9,11 +9,11 @@ const ApiCall = () => {
   const apiKey = asset.apiKey; 
   useEffect(() => {
     const getApi = async() => {
-      const request = await axios.fetch(
+      const request = await axios.get(
         `http://localhost:3000/`, 
         `https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${apiKey}&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20131101`
-      ).then((request) => {
-        setData(request.text());
+        ).then(request => {
+        setData(request.data);
       });
       //console.log(request);
     };
