e5b4526babc77a57d50ecc97a28ac3c7bbe277f6
박희진 <heejinpark10@gmail.com>
2022-09-19 22:35:57 +0900

    after studying JS asynchronous

--- src/Components/ApiCall.js
+++ src/Components/ApiCall.js
@@ -8,17 +8,16 @@ const ApiCall = () => {
   const [loading, setLoading] = useState(false);
   const apiKey = asset.apiKey; 
   useEffect(() => {
-    const getApi = async() => {
+   async function getApi(){
       const request = await axios.get(
         `http://localhost:3000/`, 
         `https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${apiKey}&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20131101`
-        ).then(request => {
-        setData(request.data);
-      });
-      console.log(request);
-    };
-    getApi();
-  }, []);
+        )
+      return request;
+      };
+      getApi().then(setData(request));
+    }, []);
+
     if(data == null) console.log('값을 불러올 수 없습니다.');
     
     console.log(data)
