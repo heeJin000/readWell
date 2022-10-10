17f77ebc0cf8fdd0778e6bc9e846e6521c072b5b
박희진 <heejinpark10@gmail.com>
2022-10-10 22:31:03 +0900

    CORS

--- src/Components/ApiCall.js
+++ src/Components/ApiCall.js
@@ -4,27 +4,20 @@ import asset from '../asset.js';
 
 
 const ApiCall = () => {
-  const [data, setData] = useState({});
-  const [error, setError] = useState(null);
-  //const [loading, setLoading] = useState(false);
+  const [data, setData] = useState({});  
   const apiKey = asset.apiKey; 
   
-
   useEffect(() => {    
-    const getApi = async() => {
-    try{  
-        const request = await axios.get(
-        `http://localhost:3000/`, 
-        `https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${apiKey}&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20131101`
-        ).then(setData(request.data));
-        
-    } catch(e) {
-      setError(e);
-    }
-  }
-    getApi();
-}, []);
-    
-  console.log(data);
+    axios({
+      method : 'GET',
+      url: `https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${apiKey}&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20131101`
+    }).then(request => {setData(request.data)});
+  }, []);
+
+  return(
+    console.log(data)
+  );
+  
+  
 } 
 export default ApiCall;
