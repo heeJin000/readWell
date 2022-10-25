d23665e4a55dcf12bffd90d0a1855c37543fec33
박희진 <heejinpark10@gmail.com>
2022-10-25 22:23:28 +0900

    json

--- src/Components/ApiCall.js
+++ src/Components/ApiCall.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useState ,useEffect } from 'react';
 import asset from '../asset.js';
+import XMLParser from "react-xml-parser";
 
 
 const ApiCall = () => {
@@ -11,14 +12,16 @@ const ApiCall = () => {
     axios({
       method : 'GET',
       url: `/ttb/api/ItemList.aspx?ttbkey=${apiKey}&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20131101`
-    }).then(request => setData(request.data))
+    }).then((request) => {
+      let xml = new XMLParser().parseFromString(request.data);
+      setData(xml);
+    })
     .catch(error => console.log(error.response));
   }, []);
 
   return(
     console.log(data)
     
-   // <li>{data.title}{data.author}</li>
 
   );
   
