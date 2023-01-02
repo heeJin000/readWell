cf095f02b768000ad6e4bcebf0d4ee51f813d75f
박희진 <heejinpark10@gmail.com>
2023-01-02 22:32:31 +0900

    ~slice is not function

--- src/Components/ApiCall.js
+++ src/Components/ApiCall.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import { useState ,useEffect } from 'react';
 import asset from '../asset.js';
+import React from 'react';
 import XMLParser from "react-xml-parser";
 
 
 const ApiCall = () => {
-  const [data, setData] = useState({});  
+  const [data, setData] = useState({ returnArr:[]});  
   const apiKey = asset.apiKey; 
   
   useEffect(() => {    
@@ -13,16 +14,13 @@ const ApiCall = () => {
       `/ttb/api/ItemList.aspx?ttbkey=${apiKey}&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20131101`
     ).then((request) => {
       let xml = new XMLParser().parseFromString(request.data);
-      setData(xml);
+      setData(
+        (prevState)=>{
+          return{...prevState, returnArr:xml.slice(0, 10)}
+        }
+      );
     })
     .catch(error => console.log(error.response));
   }, []);
-
-  return <ul> {data.map(value => {
-    <li key={value.id}> {value.title} </li>
-    })}
-  </ul>
-  
-  
-} 
+}
 export default ApiCall;
