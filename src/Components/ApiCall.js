bd5764aa4a20d353d1b0a88116b98320bfc6f956
박희진 <heejinpark10@gmail.com>
2023-02-21 22:12:40 +0900

    map and value

--- src/Components/ApiCall.js
+++ src/Components/ApiCall.js
@@ -3,25 +3,32 @@ import { useState ,useEffect } from 'react';
 import asset from '../asset.js';
 import React from 'react';
 import XMLParser from "react-xml-parser";
+import Title from './Title.js';
 
 
 const ApiCall = () => {
   const [data, setData] = useState({ returnArr:['initialization'],});  
   const apiKey = asset.apiKey; 
-  
+  let listItems = [];
+  //const [listItems, setListItems] = useState([]);
+
   useEffect(() => {    
      axios.get(
       `/ttb/api/ItemList.aspx?ttbkey=${apiKey}&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20131101`
     ).then((request) => {
       let xml = new XMLParser().parseFromString(request.data);
-      console.log('안녕하세요', xml.children); 
-      setData ( () => { 
+      //console.log('안녕하세요', xml.children);
+      setData(() => { 
         return {returnArr:xml.children.slice(11),}
-    })}
-      ).catch(error => console.log(error.response));
-    }, [])
-    return(
-      console.log(data)
+      })
+      listItems.push(data);
+    }
+  ).catch(error => console.log(error.response));
+}, [])
+    return( 
+      listItems.map((value)=>{
+        return <li>{value}</li>
+      })
     );
   };
     
